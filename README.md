40c2b58791c8768d50aea6eb906bfca3d8f2dc65
Sid <sidchopp@gmail.com>
2021-10-28 11:54:58 -0600

    Card created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# i-will-be-back" 
+"# i-will-be-back"
+
+It's a Fall back for my Heroku Apps (in production) when they cosume their free monthly hours.
--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Container, Card, Image, Icon, Divider } from 'semantic-ui-react'
+import Terminator from './terminator.jpg'
 
 function App() {
+
+  const month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+
+  const d = new Date();
+  let monthName = month[d.getMonth()];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container text textAlign='center' style={{ marginTop: '50px' }}>
+      <Card raised fluid centered>
+        <Image src={Terminator} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Hasta La Vista...{" "}User</Card.Header>
+
+          <Card.Meta>
+            <div style={{ marginTop: '15px' }} >
+              <Icon name="battery empty" />
+              {" "} My Present Mode : Stand-By
+            </div>
+            <div style={{ margin: '5px' }} >
+              <Icon name="battery full" />
+              {" "}My Functional Mode :  Starts at 00:00 hours{" "}{monthName} End </div>
+          </Card.Meta>
+          <Divider />
+          <Card.Description>
+            <p>  Actually I am using the free service from Heroku and I have consumed my entire allocation of 550 free dyno hours for
+              <b> {monthName}</b>
+              {" "}
+              <Icon name="frown"></Icon>
+            </p>
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <b>I WILL BE BACK...</b>
+        </Card.Content>
+      </Card>
+    </Container>
   );
 }
 
